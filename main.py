93e7a8ec980deaa95fe7638ece4fcabc88df5340
Lawrence <lawrencedsouza125@gmail.com>
2026-02-02 18:34:35 +0530

    get api as updated

--- main.py
+++ main.py
@@ -20,6 +20,13 @@ def read_root():
 def get_users():
     return users
 
+@app.get("/users/{user_id}")
+def get_user(user_id:int):
+    if user_id not in users:
+        raise HTTPException(status_code=404, detail="User not found")
+    
+    return users[user_id]   
+
 @app.post("/users")
 def create_user(user:User):
     if user.id in users:
