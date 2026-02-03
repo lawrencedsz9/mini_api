e0bc25186a9d79b90e9f2f6f8d93a12663635177
Lawrence <lawrencedsouza125@gmail.com>
2026-02-03 19:55:15 +0530

    added tasks by specific user

--- main.py
+++ main.py
@@ -32,6 +32,27 @@ def get_user(user_id:int):
     
     return users[user_id]   
 
+@app.get("/tasks")
+def get_tasks():
+    return tasks
+
+@app.get("/users/{user_id}/tasks")
+def get_tasks_for_user(user_id:int):
+    if user_id not in users:
+        raise HTTPException(status_code=404, detail="User not found")
+    
+    user_tasks = []
+
+    for task in tasks.values():
+        if task["user_id"] == user_id:
+            user_tasks.append(task)
+
+    if not user_tasks:
+        return {"message": "No tasks for this user"}
+    
+    return user_tasks
+
+
 @app.post("/users")
 def create_user(user:User):
     if user.id in users:
@@ -39,3 +60,14 @@ def create_user(user:User):
     
     users[user.id]= user.model_dump()
     return {"message":"User created successfully"}
+
+@app.post("/tasks")
+def create_task(task:Task):
+    if task.id in tasks:
+        raise HTTPException(status_code=400, detail="Task already exists")
+    
+    if task.user_id not in users:
+        raise HTTPException(status_code=404, detail="User not found")
+    
+    tasks[task.id]= task.model_dump()
+    return {"message":"Task created successfully"}
